5ccee94972557385f00734d19a3fe5c5df194215
Mikhail Rybolovlev <michaels17525@mail.ru>
2020-03-24 14:45:27 +0300

    Add getter functions in Target module

--- Target.py
+++ Target.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from random import randrange as rnd, choice
-import time
 
 root = Tk()
 root.geometry('800x600')
@@ -11,6 +10,7 @@ colors = ['red', 'orange', 'yellow', 'green', 'blue']
 
 
 class Target:
+    """The target class. Target can move, take death and rebirth. Each iteration have random values."""
     def __init__(self):
         self.x = rnd(100, 700)
         self.y = rnd(100, 500)
@@ -31,6 +31,30 @@ class Target:
         if self.y + self.delta_y < 0:
             self.delta_x = -1 * self.delta_x
         canv.move(self.ball, self.delta_x, self.delta_y)
+        self.x += self.delta_x
+        self.y += self.delta_y
 
     def death(self):
         canv.delete(self.ball)
+
+    def new_ball(self):
+        self.x = rnd(100, 700)
+        self.y = rnd(100, 500)
+        self.r = rnd(15, 80)
+        self.delta_x = rnd(1, 15)
+        self.delta_y = rnd(1, 15)
+        self.color = choice(colors)
+        self.points_value = 100 // self.r
+        self.ball = canv.create_oval(self.x + self.r, self.y + self.r, self.x - self.r, self.y - self.r, self.color)
+
+    @property
+    def get_x(self):
+        return self.x
+
+    @property
+    def get_y(self):
+        return self.y
+
+    @property
+    def get_r(self):
+        return self.r
--- main.py
+++ main.py
@@ -19,15 +19,16 @@ class VictoryPoints:
 
 def click(event):
     """If you catch the ball, this function will increase your score. Otherwise it print your total scores"""
-    if math.pow((math.pow(first_target.x - event.x, 2) + math.pow(first_target.y - event.y, 2)), 0.5) <= first_target.r:
+    if math.pow((math.pow(first_target.get_x() - event.x, 2) + math.pow(first_target.get_y() - event.y, 2)), 0.5)\
+            <= first_target.get_r():
         player.score += 1
     return player.score
 
 
 first_target = Target
 player = VictoryPoints()
-# player.name = input()
-# player.score = 0
+player.name = input()
+player.score = 0
 first_target_catch = True
 canv.bind('<Button-1>', click)
 mainloop()
